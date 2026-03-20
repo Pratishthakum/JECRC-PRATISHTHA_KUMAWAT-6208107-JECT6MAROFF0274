defe35806d3864c789d4c6d91f46969958aefca4
Pratishtha Kumawat <162327627+Pratishthakum@users.noreply.github.com>
2026-03-20 05:56:48 +0530

    Update Task2.py

--- 18_March/Task2.py
+++ 18_March/Task2.py
@@ -1,54 +1,46 @@
-## Task 2
-### Registration Form
-# 1. Go to: https://demoqa.com/automation-practice-form
-# 2. Handle every element in that form except the calendar
-# `Note: Give fake names and emails`
-# 3. Click on submit button
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from time import sleep
-
-opts = webdriver.ChromeOptions()
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+opts= webdriver.ChromeOptions()
 opts.add_experimental_option('detach',True)
-
 driver = webdriver.Chrome(options=opts)
-driver.get('https://demoqa.com/automation-practice-form')
+driver.get('https://qavbox.github.io/demo/signup/')
 driver.maximize_window()
-sleep(2)
-first_name = driver.find_element(By.ID,'firstName')
-first_name.send_keys('abc')
-sleep(2)
-last_name = driver.find_element(By.ID,'lastName')
-last_name.send_keys('def')
-sleep(2)
-email_ID = driver.find_element(By.ID,'userEmail')
-email_ID.send_keys('abcdefd@gmail.com')
-sleep(2)
-gender=driver.find_element(By.ID,'gender-radio-2')
-gender.click()
-sleep(2)
-phone_no = driver.find_element(By.ID,'userNumber')
-phone_no.send_keys('0123456789')
-sleep(2)
-checkboxes = driver.find_elements(By.XPATH,'//input[@type="checkbox"]')
-sleep(1)
-for i in range(0,len(checkboxes)):
-    if i==0:
-        continue
-    checkboxes[i].click()
-    sleep(1)
-upload = driver.find_element(By.ID,'uploadPicture')
-upload.send_keys(r'C:\Users\Dell\Downloads\file1.txt')
-sleep(2)
-address_filed = driver.find_element(By.ID,'currentAddress')
-address_filed.send_keys('226 asdf, sdfghjkl, hjklihgfx')
-state = driver.find_element(By.ID,'react-select-3-input')
-state.send_keys('Rajasthan',Keys.ENTER)
-city = driver.find_element(By.ID,'react-select-4-input')
-city.send_keys('Jaipur',Keys.ENTER)
-submit_button = driver.find_element(By.ID,'submit')
-submit_button.click()
-sleep(5)
+wait = WebDriverWait(driver, 5, poll_frequency=0.5)
+
+Fullname = wait.until(EC.visibility_of_element_located((By.ID, 'username')))
+Fullname.send_keys('abc')
+
+email = wait.until(EC.visibility_of_element_located((By.ID, 'email')))
+email.send_keys('abc@gmail.com')
+
+number=wait.until(EC.visibility_of_element_located((By.ID,'tel')))
+number.send_keys('7869012345')
+
+upload_file=wait.until(EC.visibility_of_element_located((By.XPATH ,'//input[@multiple="multiple"]')))
+upload_file.send_keys(r'C:\Users\Dell\Downloads\photo01.jpg')
+
+
+gender=wait.until(EC.visibility_of_element_located((By.NAME,'sgender')))
+dropdown=Select(gender)
+dropdown.select_by_value('female')
+
+YOE=wait.until(EC.visibility_of_element_located((By.XPATH,'//input[@value="above seven"]')))
+YOE.click()
+
+Skills=wait.until(EC.visibility_of_all_elements_located((By.XPATH,'//label[@for="skills"]/following-sibling::input')))
+for skill in Skills:
+    if skill.get_attribute('value')=="automationtesting" or  skill.get_attribute('value')=="java" or skill.get_attribute("value")=="API":
+        skill.click()
+
+tool=wait.until(EC.visibility_of_element_located((By.ID,'tools')))
+tools=Select(tool)
+tools.select_by_visible_text('Selenium')
+
+submit=wait.until(EC.element_to_be_clickable((By.ID,'submit')))
+submit.click()
 driver.quit()
+
